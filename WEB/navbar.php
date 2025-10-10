e4a1cf6545e73df6c1250e2444ef492dfd53570d
LeviAlcantara <llalcantara1227@gmail.com>
2025-10-10 08:19:40 -0300

    Nav 10/10 8:20
    modificação da navbar

--- WEB/navbar.php
+++ WEB/navbar.php
@@ -1,23 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <img src="../img/logo.png" class="navbar-brand img-nav">
-
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="nav nav-pills" id="pills-tab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-criar-tab" data-toggle="pill" data-target="#pills-criar" type="button" role="tab" aria-controls="pills-criar" aria-selected="true">Criar</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-consultar-tab" data-toggle="pill" data-target="#pills-consultar" type="button" role="tab" aria-controls="pills-consultar" aria-selected="false">Consultar</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-alterar-tab" data-toggle="pill" data-target="#pills-alterar" type="button" role="tab" aria-controls="pills-alterar" aria-selected="false">Alterar</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-deletar-tab" data-toggle="pill" data-target="#pills-deletar" type="button" role="tab" aria-controls="pills-deletar" aria-selected="false">Deletar</button>
-            </li>
-        </ul>
-    </div>
+    <ul class="nav nav-pills" id="pills-tab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="pills-criar-tab" data-toggle="pill" data-target="#pills-criar" type="button" role="tab" aria-controls="pills-criar" aria-selected="true">Criar</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-consultar-tab" data-toggle="pill" data-target="#pills-consultar" type="button" role="tab" aria-controls="pills-consultar" aria-selected="false">Consultar</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-alterar-tab" data-toggle="pill" data-target="#pills-alterar" type="button" role="tab" aria-controls="pills-alterar" aria-selected="false">Alterar</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-deletar-tab" data-toggle="pill" data-target="#pills-deletar" type="button" role="tab" aria-controls="pills-deletar" aria-selected="false">Deletar</button>
+        </li>
+    </ul>
 </nav>
